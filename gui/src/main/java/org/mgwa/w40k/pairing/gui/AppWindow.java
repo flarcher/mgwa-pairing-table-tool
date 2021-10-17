7bc63aa1aa9c8f6df88a4c8cd048e2b7bd3ea677
Fabrice Larcher <7803127+flarcher@users.noreply.github.com>
2021-10-17 17:30:04 +0200

    WIP: reads the matrix for a new GUI scene

--- gui/src/main/java/org/mgwa/w40k/pairing/gui/AppWindow.java
+++ gui/src/main/java/org/mgwa/w40k/pairing/gui/AppWindow.java
@@ -4,8 +4,12 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import org.mgwa.w40k.pairing.LoggerSupplier;
 import org.mgwa.w40k.pairing.gui.scene.InfoScene;
+import org.mgwa.w40k.pairing.gui.scene.MatrixSetupScene;
 import org.mgwa.w40k.pairing.gui.scene.SceneDefinition;
 import org.mgwa.w40k.pairing.gui.scene.TeamDefinitionScene;
+import org.mgwa.w40k.pairing.matrix.Matrix;
+import org.mgwa.w40k.pairing.matrix.MatrixReader;
+import org.mgwa.w40k.pairing.matrix.xls.XlsMatrixReader;
 
 import java.nio.file.Path;
 import java.util.Objects;
@@ -19,7 +23,7 @@ import java.util.logging.Logger;
  *
  * <p>Here is the method calls sequence for the start of the application:</p>
  * <ol>
- *     <li>At first the {@link #main(String[])} static method is called from the Java main method {@link Main#main(String[])}</li>
+ *     <li>At first the {@link #main()} static method is called from the Java main method {@link Main#main(String[])}</li>
  *     <li>Then through the {@link #launch(String...)} method of the JavaFX abstract class...</li>
  *     <li>Our method {@link #start(Stage)} gets finally called</li>
  * </ol>
@@ -29,7 +33,7 @@ public class AppWindow extends Application {
 	/**
 	 * See the {@link Main} class acts as the main class of the Java application in order to avoid a RuntimeException.
 	 */
-	public static void main(String[] args) {
+	public static void main() {
 		launch();
 	}
 
@@ -48,21 +52,47 @@ public class AppWindow extends Application {
 		stage.show();
 	}
 
-	private SceneDefinition getInfoScene() {
-		return new InfoScene(
-				Platform::exit,
-				String.format("Team names are %s and %s\n\nYou have%s the table choice.\n\nGiven file is: %s",
-						state.getRowTeamName(),
-						state.getColTeamName(),
-						state.youHaveTheTableToken() ? "" : " not",
-						state.getMatrixFilePath().map(Objects::toString).orElse("<none>"))
-			);
-	}
+	private TeamDefinitionScene teamDefinition;
 
     @Override
     public void start(Stage stage) {
 		this.stage = stage;
-		goToScene(new TeamDefinitionScene(() -> goToScene(getInfoScene())));
+		teamDefinition = new TeamDefinitionScene(this::toMatrixDisplay);
+		goToScene(teamDefinition);
     }
 
+	private void displayError(String msg, SceneDefinition sceneTarget) {
+		goToScene(new InfoScene(
+			() -> goToScene(sceneTarget),
+			msg));
+	}
+
+	private static final int DEFAULT_ARMY_COUNT = 3;
+
+	private void toMatrixDisplay() {
+
+		final Matrix matrix;
+		if (!state.getMatrix().isPresent()) {
+			if (state.getMatrixFilePath().isPresent()) {
+				Path path = state.getMatrixFilePath().get();
+				try (MatrixReader matrixReader = XlsMatrixReader.fromFile(path.toFile())) {
+					matrix = matrixReader.get();
+					logger.info(String.format("Using matrix of size %s", matrix.getSize()));
+				} catch (Exception e) {
+					logger.log(Level.SEVERE, "Impossible to read file", e);
+					displayError(String.format("Unable to read file %s", path), teamDefinition);
+					return;
+				}
+			} else {
+				logger.info("Using empty matrix");
+				matrix = new Matrix(DEFAULT_ARMY_COUNT);
+			}
+			state.setMatrix(matrix);
+		}
+		else {
+			matrix = state.getMatrix().get();
+		}
+
+		goToScene(new MatrixSetupScene(matrix));
+	}
 }
