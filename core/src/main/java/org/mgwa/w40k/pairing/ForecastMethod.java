1840ecbb6ee17d066a27f49418ec888179b2e18e
Fabrice Larcher <7803127+flarcher@users.noreply.github.com>
2022-04-30 12:12:02 +0200

    WIP: first implementation attempt for the scoring algorithm: to be tested

--- core/src/main/java/org/mgwa/w40k/pairing/ForecastMethod.java
+++ core/src/main/java/org/mgwa/w40k/pairing/ForecastMethod.java
@@ -1,5 +1,8 @@
 package org.mgwa.w40k.pairing;
 
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+
 /**
  * Defines how to anticipate next pairings
  */
@@ -8,10 +11,34 @@ public enum ForecastMethod {
 	/**
 	 * Uses the pairing that has the maximum score.
 	 */
-	LUCKY_BUT_RISKY,
+	LUCKY_BUT_RISKY {
+		@Override
+		public BinaryOperator<Integer> getScoreReducer() {
+			return Math::max;
+		}
+
+		@Override
+		public Function<Integer, Integer> getFinalizer(int pairCount) {
+			return Function.identity();
+		}
+	},
 	/**
 	 * Uses the pairing that maximizes the average of scores.
 	 */
-	AVERAGE,
+	AVERAGE {
+		@Override
+		public BinaryOperator<Integer> getScoreReducer() {
+			return Integer::sum;
+		}
 
+		@Override
+		public Function<Integer, Integer> getFinalizer(int pairCount) {
+			return score -> score / pairCount;
+		}
+	},
+
+	;
+
+	public abstract BinaryOperator<Integer> getScoreReducer();
+	public abstract Function<Integer, Integer> getFinalizer(int pairCount);
 }
