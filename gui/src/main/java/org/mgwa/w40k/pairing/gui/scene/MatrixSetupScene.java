b8095cfd684b74b2cf31eff13bbd95e586a0834d
Fabrice Larcher <7803127+flarcher@users.noreply.github.com>
2022-06-20 22:06:40 +0200

    Moving backward and forward from the matrix setup page

--- gui/src/main/java/org/mgwa/w40k/pairing/gui/scene/MatrixSetupScene.java
+++ gui/src/main/java/org/mgwa/w40k/pairing/gui/scene/MatrixSetupScene.java
@@ -2,6 +2,7 @@ package org.mgwa.w40k.pairing.gui.scene;
 
 import javafx.geometry.HPos;
 import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
@@ -15,9 +16,11 @@ import org.mgwa.w40k.pairing.matrix.Score;
 import org.mgwa.w40k.pairing.util.LoggerSupplier;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Function;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 /**
@@ -25,8 +28,44 @@ import java.util.stream.IntStream;
  */
 public class MatrixSetupScene extends AbstractMainScene {
 
-	public MatrixSetupScene(Function<String, String> labelGetter) {
-		super(labelGetter, 1);
+	public MatrixSetupScene(Function<String, String> labelGetter, Runnable previous, Runnable next) {
+		super(labelGetter, 3);
+		this.previousAction = Objects.requireNonNull(previous);
+		this.nextAction = Objects.requireNonNull(next);
+	}
+
+	private final Runnable previousAction;
+	private final Runnable nextAction;
+
+	private static List<TextField> textFieldOfNames(List<Army> armies) {
+		int nodeWidth = SCENE_WIDTH / (armies.size() + 1);
+		return armies.stream()
+			.map(army -> {
+				TextField text = new TextField();
+				text.setText(army.getName());
+				text.maxWidth(nodeWidth);
+				GridPane.setHalignment(text, HPos.CENTER);
+				return text;
+			})
+			.collect(Collectors.toList());
+	}
+
+	private static List<Army> withNewNames(List<Army> armies, List<TextField> newNames) {
+		if (armies.size() != newNames.size()) {
+			throw new IllegalStateException();
+		}
+		return IntStream.range(0, armies.size())
+			.mapToObj(index -> {
+				Army originalArmy = armies.get(index);
+				TextField newName = newNames.get(index);
+				return Army.renamed(originalArmy, newName.getText());
+			})
+			.collect(Collectors.toList());
+	}
+
+	private static void updateState(Matrix matrix, List<TextField> rowArmyNames, List<TextField> colArmyNames) {
+		matrix.setArmies(true, withNewNames(matrix.getArmies(true), rowArmyNames));
+		matrix.setArmies(false, withNewNames(matrix.getArmies(false), colArmyNames));
 	}
 
 	@Override
@@ -36,24 +75,16 @@ public class MatrixSetupScene extends AbstractMainScene {
 		Matrix matrix = state.getMatrix().orElseThrow();
 		List<Army> rowArmies = matrix.getArmies(true);
 		List<Army> colArmies = matrix.getArmies(false);
+		List<TextField> rowArmyNames = textFieldOfNames(rowArmies);
+		List<TextField> colArmyNames = textFieldOfNames(colArmies);
 		GridPane grid = NodeFactory.createGrid(matrix.getSize() + 1);
+		grid.setGridLinesVisible(true);
 		int nodeWidth = SCENE_WIDTH / (matrix.getSize() + 1);
 		IntStream.range(0, matrix.getSize())
 			.forEach(i -> {
 
-				String colArmyName = colArmies.get(i).getName();
-				TextField text = new TextField();
-				text.setText(colArmyName);
-				text.maxWidth(nodeWidth);
-				GridPane.setHalignment(text, HPos.CENTER);
-				grid.add(text, i + 1, 0, 1, 1);
-
-				String rowArmyName = rowArmies.get(i).getName();
-				text = new TextField();
-				text.setText(rowArmyName);
-				text.maxWidth(nodeWidth);
-				GridPane.setHalignment(text, HPos.CENTER);
-				grid.add(text, 0, i + 1, 1, 1);
+				grid.add(colArmyNames.get(i), i + 1, 0, 1, 1);
+				grid.add(rowArmyNames.get(i), 0, i + 1, 1, 1);
 
 				for (int j = matrix.getSize() - 1; j >= 0; j--) {
 					final int rowIndex = i;
@@ -90,10 +121,23 @@ public class MatrixSetupScene extends AbstractMainScene {
 				}
 			});
 
-		addRow(HPos.CENTER, grid);
+		addNode(grid, getRowIndex(), 0, 1, 3, HPos.CENTER);
 
-		// TODO: Add a previous button
-		// TODO: Add a next button (update the army names !)
+		newRow();
+		Button prevButton = NodeFactory.createButton(
+			labelGetter.apply("previous"),
+			e -> {
+				updateState(matrix, rowArmyNames, colArmyNames);
+				previousAction.run();
+			});
+		addNode(prevButton, getRowIndex(), 0, 1, 1, HPos.LEFT);
+		Button nextButton = NodeFactory.createButton(
+			labelGetter.apply("next"),
+			e -> {
+				updateState(matrix, rowArmyNames, colArmyNames);
+				nextAction.run();
+			});
+		addNode(nextButton, getRowIndex(), 2, 1, 1, HPos.RIGHT);
 	}
 
 }
