a5e881279080871ca003aa9d3ad2da83f1ef8003
Fabrice Larcher <flarcher6@gmail.com>
2023-08-15 08:50:58 +0200

    API returns the match setup

---
+++ http-api/src/main/java/org/mgwa/w40k/pairing/api/model/Match.java
@@ -0,0 +1,34 @@
+package org.mgwa.w40k.pairing.api.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.annotation.concurrent.Immutable;
+
+@Immutable
+public class Match {
+
+    public Match(String rowTeamName, String columnTeamName, int teamMemberCount) {
+        this.rowTeamName = rowTeamName;
+        this.columnTeamName = columnTeamName;
+        this.teamMemberCount = teamMemberCount;
+    }
+
+    @JsonProperty("row_team")
+    private final String rowTeamName;
+    @JsonProperty("column_team")
+    private final String columnTeamName;
+    @JsonProperty("team_member_count")
+    private final int teamMemberCount;
+
+    public String getRowTeamName() {
+        return rowTeamName;
+    }
+
+    public String getColumnTeamName() {
+        return columnTeamName;
+    }
+
+    public int getTeamMemberCount() {
+        return teamMemberCount;
+    }
+}
