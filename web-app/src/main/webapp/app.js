df32f21cdb7555ffeffe814b2783144d432f49e0
Fabrice Larcher <7803127+flarcher@users.noreply.github.com>
2023-08-16 11:38:12 +0200

    Displaying scores matrix from web-app

--- web-app/src/main/webapp/app.js
+++ web-app/src/main/webapp/app.js
@@ -4,11 +4,12 @@ var defaultPort = 8000;
 // Error handling related to API calls
 var errorHandler = function(jsonResponse) {
     var cause = jsonResponse["cause"];
+    var message = jsonResponse["message"] || "?"
     if (cause === "network") {
         // Display error
         var section = switchSection("no_api");
         section.querySelector('#port').innerHTML = port.toFixed();
-        section.querySelector('#reason').innerHTML = jsonResponse["message"]
+        section.querySelector('#reason').innerHTML = message
         // Optional retry
         var hashPort = window.pairing_api.port;
         if (hashPort != defaultPort) {
@@ -20,7 +21,9 @@ var errorHandler = function(jsonResponse) {
         }
     }
     else {
-        // TODO
+        var section = switchSection("api_error");
+        section.querySelector('#status').innerHTML = (jsonResponse["status"] || 0).toFixed();
+        section.querySelector('#message').innerHTML = message
     }
 }
 
@@ -39,11 +42,53 @@ var getApiUrl = function() {
     return apiUrlPrefix;
 }
 
+var refreshMatrix = function() {
+    var matrixTableBody = document.querySelector("#matrix > table > tbody");
+
+    emptyElement(matrixTableBody); // reset
+
+    var newRow = document.createElement("tr");
+    newRow.classList.add('names');
+    matrixTableBody.appendChild(newRow);
+    const cornerCell = document.createElement("td");
+    cornerCell.classList.add('corner');
+    newRow.appendChild(cornerCell);
+    
+    var columnArmies = getData().col_armies; // Column armies names
+    var rowArmies    = getData().row_armies; // Row armies names
+    if (columnArmies.length != rowArmies.length) {
+        throw "Inconsistent matrix size"
+    }
+
+    for (let i = 0; i < columnArmies.length; i++) {
+        let colArmyCell = document.createElement("td");
+        colArmyCell.textContent = columnArmies[i];
+        newRow.appendChild(colArmyCell);
+    }
+
+    var scores    = getData().scores;     // Matrix values
+    for (let i = 0; i < rowArmies.length; i++) {
+        newRow = document.createElement("tr");
+        matrixTableBody.appendChild(newRow);
+        let rowArmyCell = document.createElement("td");
+        rowArmyCell.classList.add('name');
+        rowArmyCell.textContent = rowArmies[i];
+        newRow.appendChild(rowArmyCell);
+        for (let j = 0; j < columnArmies.length; j++) {
+            let scoreCell = document.createElement("td");
+            scoreCell.classList.add('score');
+            let score = scores[i][j];
+            scoreCell.textContent = score.min.toFixed() + "-" + score.max.toFixed();
+            newRow.appendChild(scoreCell);
+        }
+    }
+};
+
 // Initialization
 window.addEventListener("load", function() {
 
         // Browser support check
-        if (!window.fetch) {
+        if (!window.fetch || !Promise.all) {
 			switchSection("not_supported"); // Warning about non compatible browser
             return;
 		}
@@ -51,12 +96,33 @@ window.addEventListener("load", function() {
         // Loading screen
         switchSection("loading");
 
-        // Getting API port
+        // Getting API URL
         var apiURl = getApiUrl();
 
-        // First API call
-        callGet(apiURl + 'match', json => {
-			switchSection("ready");
-		}, errorHandler);
+        // API calls
+        allGetCalls([
+                apiURl + 'match',
+                apiURl + 'match/rows',
+                apiURl + 'match/cols',
+                apiURl + 'scores'
+            ],
+            jsonResults => {
+                // Assign data
+                getData().match      = jsonResults[0];
+                getData().row_armies = jsonResults[1];
+                getData().col_armies = jsonResults[2];
+                getData().scores     = jsonResults[3];
+
+                // Refresh the DOM
+                refreshMatrix();
+
+                // Show the section
+                var section = switchSection("ready");
+                setupNavBar(section);
+            },
+            errors => {
+                errors.forEach(error => console.error("API call error: " + error));
+                errorHandler(errors[0]); // We only display the first error for simplicity
+            });
         
 	}, true);
--- web-app/src/main/webapp/utils.js
+++ web-app/src/main/webapp/utils.js
@@ -1,3 +1,8 @@
+// Variable that contains the application data
+window.app_data = {};
+var getData = function() {
+    return window.app_data;
+}
 
 /*
  * Switch from a step to another
@@ -13,13 +18,43 @@ var switchSection = function(id) {
 };
 
 /*
+ * Switch from a navigation tab to another
+ * using `nav > a` anchors corresponding `div` elements.
+ */
+var setupNavBar = function(section) {
+	document.querySelector('nav').classList.remove('hidden');
+    document.querySelectorAll('nav > a').forEach(anchor => {
+            var linkTarget = anchor.dataset.target;
+            var targetedElement = section.querySelector('div#'+linkTarget);
+            if (!targetedElement) {
+                throw "No target element #" + linkTarget + " found!";
+            }
+			anchor.addEventListener('click', e => {
+					section.querySelectorAll('div.current').forEach(s => {
+						s.classList.remove('current');
+					});
+					document.querySelectorAll('nav > a.selected').forEach(a => {
+						a.classList.remove('selected');
+					});
+					anchor.classList.add('selected');
+                    targetedElement.classList.add('current');
+                });
+        });
+}
+
+function emptyElement(element) {
+	while (element.firstElementChild) {
+	   element.firstElementChild.remove();
+	}
+}
+
+/*
  * Calls a GET request to the API
  */
-var callGet = (url, thenFn, errorFn) => {
+var getCall = (url, thenFn, errorFn) => {
 	fetch(url)
 		.then((response) => {
 			if (!response.ok) {
-				console.error('GET call failed with status ' + response.status);
                 response.json().then(errorFn);
 			}
 			else {
@@ -28,5 +63,24 @@ var callGet = (url, thenFn, errorFn) => {
 		})
 		.catch((error) => {
 			errorFn({ "cause": "network", "message": error.message });
+		});
+};
+
+/*
+ * Multiple calls to the API
+ */
+var allGetCalls = (manyUrls, thenFn, errorFn) => {
+	Promise.all(manyUrls.map(url => fetch(url)))
+		.then((responses) => {
+			var inErrorResponses = responses.filter((rs) => !rs.ok);
+			var hasAnyError = inErrorResponses.length > 0;
+			Promise.all(
+				(hasAnyError ? inErrorResponses : responses)
+					.map(rs => rs.json()))
+				.then(hasAnyError ? errorFn : thenFn);
+		})
+		.catch((error) => {
+			// First error (fail fast)
+			errorFn([ { "cause": "network", "message": error.message } ]);
 		});
 };
