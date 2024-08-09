a9a00e51a5e85636be43995288bd0c65d80ddbac
Fabrice Larcher <7803127+flarcher@users.noreply.github.com>
2024-08-09 14:06:54 +0200

    Debug mode that allow a reload of the web-app without stopping the API

--- http-api/src/main/java/org/mgwa/w40k/pairing/api/service/StatusService.java
+++ http-api/src/main/java/org/mgwa/w40k/pairing/api/service/StatusService.java
@@ -1,8 +1,10 @@
 package org.mgwa.w40k.pairing.api.service;
 
 import org.mgwa.w40k.pairing.api.model.AppStatus;
+import org.mgwa.w40k.pairing.util.LoggerSupplier;
 
 import java.util.Objects;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicReference;
 
 /**
@@ -12,9 +14,13 @@ public class StatusService {
 
     public StatusService(Runnable onExit) {
         this.onExit = Objects.requireNonNull(onExit);
+        this.dryExit = Optional.ofNullable(System.getenv("API_SERVER_DEBUG"))
+                .map(envValue -> !envValue.equalsIgnoreCase("false"))
+                .orElse(false);
     }
 
     private final Runnable onExit;
+    private final boolean dryExit;
     private final AtomicReference<AppStatus> currentStatus = new AtomicReference<>(AppStatus.INITIALIZING);
 
     public AppStatus getStatus() {
@@ -27,10 +33,13 @@ public class StatusService {
 
     public boolean exiting() {
         AppStatus oldStatus = currentStatus.getAndUpdate( status -> {
-            if (status != AppStatus.EXITING) {
+            if (status != AppStatus.EXITING && !dryExit) {
                 onExit.run();
             }
-            return AppStatus.EXITING; // New status
+            if (dryExit) {
+                LoggerSupplier.INSTANCE.getLogger().warning("Ignoring exit request (debug mode)");
+            }
+            return dryExit ? AppStatus.RUNNING : AppStatus.EXITING; // New status
         });
         return oldStatus != AppStatus.EXITING;
     }
--- launch.sh
+++ launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Launch the project without any packaging required
-
+export API_SERVER_DEBUG=true
 cd main
 mvn javafx:run
 cd - > /dev/null
