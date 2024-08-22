e9378322703c9b1bb8abf91f26e79d80f9ab7592
Fabrice Larcher <7803127+flarcher@users.noreply.github.com>
2024-08-22 14:38:57 +0200

    Code cleanup in pairing.js

--- web-app/src/main/webapp/pairing.js
+++ web-app/src/main/webapp/pairing.js
@@ -1,16 +1,4 @@
 
-var refreshAssignmentForm = function() {
-    var tablesDiv = document.getElementById("assign");
-    var teamNamesRow = tablesDiv.querySelectorAll(".teams .name > span");
-    if (teamNamesRow.length != 2) {
-        throw "Expecting 2 teams";
-    }
-    teamNamesRow[0].textContent = getData().match.row_team
-    teamNamesRow[1].textContent = getData().match.column_team
-
-    // TODO
-};
-
 // Returns indexes of armies that were not assigned to a table
 var getRemainingArmies = function(isRow) {
     let   data         = getData();
@@ -27,54 +15,6 @@ var getRemainingArmyNames = function(isRow) {
     const allArmyNames = isRow ? data.row_armies : data.col_armies;
     const remainingIndexes = getRemainingArmies(isRow);
     return remainingIndexes.map(index => allArmyNames[index]);
-};
-
-var triggerAnalysis = function(isRowArmy, armyName) {
-    const waitingLoop = document.getElementById('analysis').querySelector('.waiting');
-    waitingLoop.classList.remove('hidden');
-
-    // TODO: API call
-}
-
-var refreshArmyList = function(isRowArmy, parentDiv) {
-    var filteredList = getRemainingArmies(isRowArmy);
-    emptyElement(parentDiv);
-    var armyList = isRow ? getData().row_armies : getData().col_armies;
-    filteredList.forEach(armyIndex => {
-        var armyName = armyList[armyIndex];
-        var candidateElement = document.createElement('span');
-        candidateElement.classList.add('army');
-        candidateElement.textContent = armyName;
-        parentDiv.appendChild(candidateElement);
-        candidateElement.addEventListener('click', e => {
-            triggerAnalysis(isRowArmy, armyName);
-        });
-    });
-};
-
-var initAnalysisForm = function() {
-    const section = document.getElementById('analysis');
-    const armyDiv = section.querySelector('.selector > div');
-    section.querySelectorAll('.teams input').forEach(radioInput => {
-        if (radioInput.checked) {
-            refreshArmyList(radioInput.value === "row", armyDiv); // Otherwise "column"
-        }
-        radioInput.addEventListener('click', e => {
-            refreshArmyList(e.target.value === "row", armyDiv); // Otherwise "column"
-        });
-    });
-};
-
-var refreshAnalysisForm = function() {
-    var formDiv = document.getElementById("analysis");
-    var teamNamesRow = formDiv.querySelectorAll("fieldset.teams label");
-    if (teamNamesRow.length != 2) {
-        throw "Expecting 2 teams";
-    }
-    teamNamesRow[0].textContent = getData().match.row_team
-    teamNamesRow[1].textContent = getData().match.column_team
-
-    // TODO
 };
 
 var checkMatchSetup = function() {
